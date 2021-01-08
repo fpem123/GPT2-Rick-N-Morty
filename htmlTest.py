c759e3876a74dfdef32b68051d8702999a8bdbad
LeeHoseop <ghtjq3909@naver.com>
2021-01-08 22:24:29 +0900

    Add html test code

---
+++ htmlTest.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+
+##
+# Get post request page.
+@app.route('/Rick-N-Morty', methods=['POST'])
+def generate():
+    return jsonify({'0': [['Rick', 'hey'], ['Morty', 'What?'], ['Jerry', '매롱'], ['Rick', 'stop'], ['Beth', "What? What is this? I don't wanna die! stop!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"]]})
+
+
+##
+# Main page.
+@app.route('/')
+def main():
+    return render_template('main.html'), 200
+
+
+if __name__ == '__main__':
+    from waitress import serve
+    serve(app, port=80, host='0.0.0.0')
